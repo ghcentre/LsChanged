90e2febc35cac1115f95835cfa6e473117f4ebe7
George Harder <ghcentre@gmail.com>
2023-03-04 20:34:57 +0300

    Fix OptionRuleProvider

--- LsChanged/CommandLine/OptionRuleProvider.cs
+++ LsChanged/CommandLine/OptionRuleProvider.cs
@@ -6,139 +6,133 @@ namespace LsChanged.CommandLine;
 
 internal class OptionRuleProvider
 {
-    public static IEnumerable<OptionRule> Rules
+    private static readonly OptionRule[] _rules = new[]
     {
-        get
-        {
-            var rules = new[]
+        #region Scan
+
+        new OptionRule("scan", 1,
+            (o, a) =>
             {
-                #region Scan
+                o.SetCommand(Command.Scan);
+                o.ScanPath = a.First();
+            }),
 
-                new OptionRule("scan", 1,
-                    (o, a) =>
+        new OptionRule("-fs", 1,
+            (o, a) =>
+            {
+                o.FollowSymlinksMode = Enum.Parse<FollowSymlinksMode>(a.First());
+            }),
+
+        #endregion
+
+        #region Compare
+
+        new OptionRule("compare", 1,
+            (o, a) =>
+            {
+                o.SetCommand(Command.Compare);
+                o.CompareOutputFile = a.First();
+            }),
+
+        new OptionRule("-cm", 1,
+            (o, a) =>
+            {
+                string modeString = a.First();
+                if (modeString == "lp")
+                {
+                    o.CompareMode = CompareMode.LastPrevious;
+                    return;
+                }
+                if (modeString == "lf")
+                {
+                    o.CompareMode = CompareMode.LastFirst;
+                    return;
+                }
+
+                if (modeString.Count(c => c == ',') == 1)
+                {
+                    string[] snapshotOrdinalStrings = modeString
+                        .Split(',')
+                        .Where(x => x.All(c => char.IsAsciiDigit(c)))
+                        .ToArray();
+                    if (snapshotOrdinalStrings.Length == 2)
                     {
-                        o.SetCommand(Command.Scan);
-                        o.ScanPath = a.First();
-                    }),
+                        o.CompareMode = CompareMode.SpecifiedSnapshots;
 
-                new OptionRule("-fs", 1,
-                    (o, a) =>
+                        o.NewCompareSnapshot = int.Parse(snapshotOrdinalStrings[0]);
+                        o.OldCompareSnapshot = int.Parse(snapshotOrdinalStrings[1]);
+
+                        return;
+                    }
+                }
+                throw new CommandLineParseException(
+                    "Compare mode option must be lp, lf, or exactly two numeric arguments separated by comma.");
+            }),
+
+        new OptionRule("-st", 1,
+            (o, a) =>
+            {
+                var fileStates = CompareFileStates.None;
+                string[] states = a.First().Split(",");
+                foreach (string stateString in states)
+                {
+                    var state = stateString switch
                     {
-                        o.FollowSymlinksMode = Enum.Parse<FollowSymlinksMode>(a.First());
-                    }),
+                        "a" => CompareFileStates.Added,
+                        "m" => CompareFileStates.Modified,
+                        "u" => CompareFileStates.Unmodified,
+                        "d" => CompareFileStates.Deleted,
+                        _ => throw new CommandLineParseException(
+                                "Compare file states requires any combination of a,m,u,d in any order.")
+                    };
+                    fileStates |= state;
+                }
+                o.CompareFileStates = fileStates;
+            }),
 
-                #endregion
+        new OptionRule("-rp", 1,
+            (o, a) =>
+            {
+                o.CompareRelativePath = a.First();
+            }),
 
-                #region Compare
+	    #endregion
 
-                new OptionRule("compare", 1,
-                    (o, a) =>
-                    {
-                        o.SetCommand(Command.Compare);
-                        o.CompareOutputFile = a.First();
-                    }),
+        #region List
 
-                new OptionRule("-cm", 1,
-                    (o, a) =>
-                    {
-                        string modeString = a.First();
-                        if (modeString == "lp")
-                        {
-                            o.CompareMode = CompareMode.LastPrevious;
-                            return;
-                        }
-                        if (modeString == "lf")
-                        {
-                            o.CompareMode = CompareMode.LastFirst;
-                            return;
-                        }
+		new OptionRule("list", 0,
+            (o, _) =>
+            {
+                o.SetCommand(Command.List);
+            }),
 
-                        if (modeString.Count(c => c == ',') == 1)
-                        {
-                            string[] snapshotOrdinalStrings = modeString
-                                .Split(',')
-                                .Where(x => x.All(c => char.IsAsciiDigit(c)))
-                                .ToArray();
-                            if (snapshotOrdinalStrings.Length == 2)
-                            {
-                                o.CompareMode = CompareMode.SpecifiedSnapshots;
-
-                                o.NewCompareSnapshot = int.Parse(snapshotOrdinalStrings[0]);
-                                o.OldCompareSnapshot = int.Parse(snapshotOrdinalStrings[1]);
-
-                                return;
-                            }
-                        }
-                        throw new CommandLineParseException(
-                            "Compare mode option must be lp, lf, or exactly two numeric arguments separated by comma.");
-                    }),
-
-                new OptionRule("-st", 1,
-                    (o, a) =>
-                    {
-                        var fileStates = CompareFileStates.None;
-                        string[] states = a.First().Split(",");
-                        foreach (string stateString in states)
-                        {
-                            var state = stateString switch
-                            {
-                                "a" => CompareFileStates.Added,
-                                "m" => CompareFileStates.Modified,
-                                "u" => CompareFileStates.Unmodified,
-                                "d" => CompareFileStates.Deleted,
-                                _ => throw new CommandLineParseException(
-                                        "Compare file states requires any combination of a,m,u,d in any order.")
-                            };
-                            fileStates |= state;
-                        }
-                        o.CompareFileStates = fileStates;
-                    }),
-
-                new OptionRule("-rp", 1,
-                    (o, a) =>
-                    {
-                        o.CompareRelativePath = a.First();
-                    }),
-
-	            #endregion
-
-                #region List
-
-		        new OptionRule("list", 0,
-                    (o, _) =>
-                    {
-                        o.SetCommand(Command.List);
-                    }),
-
-	            #endregion
+	    #endregion
 
 
-                new OptionRule("delete", 1,
-                    (o, a) =>
-                    {
-                        throw new NotImplementedException();
-                    }),
+        new OptionRule("delete", 1,
+            (o, a) =>
+            {
+                throw new NotImplementedException();
+            }),
 
-                new OptionRule("clear", 0,
-                    (o, _) =>
-                    {
-                        throw new NotImplementedException();
-                    }),
+        new OptionRule("clear", 0,
+            (o, _) =>
+            {
+                throw new NotImplementedException();
+            }),
 
-               new OptionRule("-s", 1,
-                    (o, a) =>
-                    {
-                        o.StorePath = a.First();
-                    }),
+        new OptionRule("-s", 1,
+            (o, a) =>
+            {
+                o.StorePath = a.First();
+            }),
 
-                new OptionRule("-v", 0,
-                    (o, _) =>
-                    {
-                        o.Verbose = true;
-                    }),
-            };
+        new OptionRule("-v", 0,
+            (o, _) =>
+            {
+                o.Verbose = true;
+            }),
+    };
 
-            return rules;
-        }
-    }
+    public static IEnumerable<OptionRule> Rules => _rules;
 }
