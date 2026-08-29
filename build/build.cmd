d7f1db9f99fd98ff5a2eaef63c022c4a9c61fc99
George Harder <ghcentre@gmail.com>
2026-08-29 16:06:10 +0300

    Add build scripts

--- build/build.cmd
+++ build/build.cmd
@@ -7,14 +7,40 @@ dotnet publish -c Release ^
                --self-contained ^
                --runtime win-x64 ^
                -p:PublishTrimmed=true ^
-               -p:PublishSingleFile=true
+               -p:PublishSingleFile=true ^
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release ^
+               -o ..\..\releases\win-arm64 ^
+               --self-contained ^
+               --runtime win-arm64 ^
+               -p:PublishTrimmed=true ^
+               -p:PublishSingleFile=true ^
+               -p:InvariantGlobalization=true
 
 dotnet publish -c Release ^
                -o ..\..\releases\linux-x64 ^
                --self-contained ^
                --runtime linux-x64 ^
                -p:PublishTrimmed=true ^
-               -p:PublishSingleFile=true
+               -p:PublishSingleFile=true ^
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release ^
+               -o ..\..\releases\linux-arm ^
+               --self-contained ^
+               --runtime linux-arm ^
+               -p:PublishTrimmed=true ^
+               -p:PublishSingleFile=true ^
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release ^
+               -o ..\..\releases\linux-arm64 ^
+               --self-contained ^
+               --runtime linux-arm64 ^
+               -p:PublishTrimmed=true ^
+               -p:PublishSingleFile=true ^
+               -p:InvariantGlobalization=true
 
 popd             
 
---
+++ build/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+pushd ../src/lschanged>/dev/null
+
+dotnet publish -c Release \
+               -o ../../releases/win-x64 \
+               --self-contained \
+               --runtime win-x64 \
+               -p:PublishTrimmed=true \
+               -p:PublishSingleFile=true \
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release \
+               -o ../../releases/win-arm64 \
+               --self-contained \
+               --runtime win-arm64 \
+               -p:PublishTrimmed=true \
+               -p:PublishSingleFile=true \
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release \
+               -o ../../releases/linux-x64 \
+               --self-contained \
+               --runtime linux-x64 \
+               -p:PublishTrimmed=true \
+               -p:PublishSingleFile=true \
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release \
+               -o ../../releases/linux-arm \
+               --self-contained \
+               --runtime linux-arm \
+               -p:PublishTrimmed=true \
+               -p:PublishSingleFile=true \
+               -p:InvariantGlobalization=true
+
+dotnet publish -c Release \
+               -o ../../releases/linux-arm64 \
+               --self-contained \
+               --runtime linux-arm64 \
+               -p:PublishTrimmed=true \
+               -p:PublishSingleFile=true \
+               -p:InvariantGlobalization=true
+
+popd             
